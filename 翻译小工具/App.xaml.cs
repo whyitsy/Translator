f5f812b7332272781e5f99a08e2acacbd4229bb0
kakoyo <kakoyo@qq.com>
2024-10-10 23:08:38 +0800

    添加项目文件。

---
+++ 翻译小工具/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace 翻译小工具
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
