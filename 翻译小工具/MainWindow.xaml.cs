630aa5c164d6e08d05a3a82be935cffffff43223
kakoyo <kakoyo@qq.com>
2024-10-13 01:17:02 +0800

    添加程序的Icon，调整和添加Memo为MVVM

--- 翻译小工具/MainWindow.xaml.cs
+++ 翻译小工具/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
-using 翻译小工具.Models;
-using 翻译小工具.Utils;
+using 翻译小工具.ViewModels;
 
 namespace 翻译小工具
 {
@@ -12,63 +10,25 @@ namespace 翻译小工具
     /// </summary>
     public partial class MainWindow
     {
-
         public MainWindow()
         {
             InitializeComponent();
-
-            Loaded += LoadMemoList;
         }
 
-        private void LoadMemoList(object sender, RoutedEventArgs e)
-        {
-            using var streamReader = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "记录.txt"));
-            // 逐行读取文件内容，直到文件末尾
-          
-            while (streamReader.ReadLine() is { } line)
-            {
-                ListBoxInRB.Items.Add(line);
-            }
-        }
-
-
-        private void ShutDownButtonClicked(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown(0);
-        }
-
-        private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void WindowDragMove(object sender, MouseButtonEventArgs e)
         {
             DragMove();
         }
 
-        private void SwitchIsTopWindow(object sender, RoutedEventArgs e)
+        private void ShutDownApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void SwitchIsTop(object sender, RoutedEventArgs e)
         {
             this.Topmost = !this.Topmost;
         }
-
-
-        private void SaveMemoClick(object sender, RoutedEventArgs e)
-        {
-
-            using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "记录.txt")))
-            {
-                writer.WriteLine(MemoText.Text);
-                ListBoxInRB.Items.Add(MemoText.Text);
-            }
-            MemoText.Text = "";
-        }
-
-        private void ChangeMemoListVisibility(object sender, RoutedEventArgs e)
-        {
-            MemoList.Visibility = MemoList.Visibility switch
-            {
-                Visibility.Visible => Visibility.Hidden,
-                Visibility.Hidden => Visibility.Visible,
-                _ => MemoList.Visibility
-            };
-        }
-
 
     }
 }
---
+++ 翻译小工具/ViewModels/MemoViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace 翻译小工具.ViewModels;
+
+public partial class MemoViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool _memoVisibility = true;
+
+    [ObservableProperty]
+    private string _memoText = string.Empty;
+
+    public ObservableCollection<string> MemoList { get; set; } = new ObservableCollection<string>();
+
+    public MemoViewModel()
+    {
+        LoadMemoData();
+    }
+
+    private void LoadMemoData()
+    {
+        MemoList.Clear();
+        using StreamReader reader = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "记录.txt"));
+        while (reader.ReadLine() is { } line)
+        {
+            MemoList.Add(line);
+        }
+    }
+
+    [RelayCommand]
+    private void ChangeMemoBoxVisibility()
+    {
+        MemoVisibility = !MemoVisibility;
+    }
+
+    [RelayCommand]
+    private void AddMemoItem()
+    {
+        using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "记录.txt"), append: true))
+        {
+            writer.WriteLine(MemoText);
+        }
+
+        MemoList.Add(MemoText);
+        MemoText = "";
+    }
+
+}
